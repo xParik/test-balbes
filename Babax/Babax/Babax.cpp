1cb8f4b24ba6003e782b9ad21e090fea8e619ddb
xParik <157728760+xParik@users.noreply.github.com>
2024-10-15 20:29:45 +0300

    Maze
    Dont colision

--- Babax/Babax/Babax.cpp
+++ Babax/Babax/Babax.cpp
@@ -1,6 +1,20 @@
 ﻿#include <SDL.h>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// Структура для стен лабиринта 
+struct Wall {
+    SDL_Rect rect;
+};
+
+// Функция для отрисовки лабиринта 
+void drawMaze(SDL_Renderer* renderer, const vector<Wall>& walls) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет для стен 
+    for (const auto& wall : walls) {
+        SDL_RenderFillRect(renderer, &wall.rect);
+    }
+}
 
 int main(int argc, char* argv[]) {
     // Инициализация SDL
@@ -37,18 +51,51 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
+
     SDL_Texture* characterTexture = SDL_CreateTextureFromSurface(renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
 
-    SDL_Rect characterRect = { 100, 100, 64, 64 }; // Положение и размер персонажа
+    SDL_Rect characterRect = { 20, 35, 64, 64 }; // Положение и размер персонажа
     const int speed = 1; // Скорость перемещения
 
-    int windowWight = 800;
+    int windowWidth = 800;
     int windowHeight = 600;
+
+    // Создаем лабиринт (стены) 
+    vector<Wall> walls = {
+    {{0, 0, 10, 300}}, //левая стена
+    {{0, 590, 3000, 10}},  //нижняя стена
+    {{0, 0, 3000, 10}},  //верхняя стена
+    {{100, 0, 10, 300}}, //правая стена
+    {{0, 300, 10, 300}}, //левая стена1
+    {{100, 390, 300, 10}},  //нижняя стена1
+    {{100, 300, 300, 10}},  //верхняя стена1
+    {{300, 400, 10, 300}}, //правая стена1
+    {{400, 160, 10, 150}}, //левая стена2
+    {{500, 390, 300, 10}},  //нижняя стена2
+    {{500, 300, 300, 10}},  //верхняя стена2
+    {{790, 0, 10, 500}}, //правая стена2
+    {{400, 390, 10, 100}}, //левая стена3
+    {{500, 500, 200, 10}},  //нижняя стена3
+    {{400, 150, 300, 10}},  //верхняя стена3
+    {{100, 400, 10, 100}}, //правая стена3
+    {{100, 390, 300, 10}},  //нижняя стена1
+    {{100, 300, 300, 10}},  //верхняя стена1
+    {{300, 400, 10, 300}}, //правая стена1
+    {{400, 160, 10, 150}}, //левая стена2
+    {{500, 390, 300, 10}},  //нижняя стена2
+    {{500, 300, 300, 10}},  //верхняя стена2
+    {{790, 0, 10, 500}}, //правая стена2
+    {{400, 390, 10, 100}}, //левая стена3
+    {{500, 500, 200, 10}},  //нижняя стена3
+    {{400, 150, 300, 10}},  //верхняя стена3
+    {{100, 400, 10, 100}} //правая стена3
+    };
 
     // Основной цикл игры
     bool isRunning = true;
     SDL_Event event;
+
 
     while (isRunning) {
         // Обрабатываем события
@@ -78,6 +125,9 @@ int main(int argc, char* argv[]) {
         // Очищаем экран
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Черный фон
         SDL_RenderClear(renderer);
+
+        // Отрисовка лабиринта 
+        drawMaze(renderer, walls);
 
         // Отрисовка персонажа
         SDL_RenderCopy(renderer, characterTexture, NULL, &characterRect);
